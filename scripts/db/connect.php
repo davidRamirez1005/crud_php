b1f4d594436c27eba14c1fe94e701135d7acae92
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-06-23 16:17:12 -0500

    test: :white_check_mark: conexion exitosa a la base de datos
    se hace el try catch y se llenan las credenciales

--- scripts/db/connect.php
+++ scripts/db/connect.php
@@ -2,8 +2,16 @@
 
 namespace App;
 
-class connect{
-    function __construct(){
-        echo "hola mundo";
+class connect extends credentials{
+    protected $con;
+    function __construct(private $dns = "mysql",private $port = 3306){
+        try {
+            $this->con=new \PDO( $this->dns.":host=".$this->__get('host').";dbname=".$this->__get('dbname').";user=". $this->username.";password=".$this->__get('password').";port=". $this->port);
+
+            print_r("conexion exitosa");
+
+        } catch (\Throwable $e) {
+            print_r($e->getMessage());
+        }
     }
 }
---
+++ scripts/system.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace App;
+
+trait system{
+    public function __get($name){
+        return $this->{$name};
+    }
+}
